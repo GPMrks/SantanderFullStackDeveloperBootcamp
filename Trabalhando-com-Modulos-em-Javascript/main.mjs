0cd7606803e55ceb15b08981c0027a6345386c63
GPMrks <guilhermepereiramarques@hotmail.com>
2022-06-14 15:45:41 -0300

    ADD - Trabalhando com Módulos em Javascript

---
+++ Trabalhando-com-Modulos-em-Javascript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Guilherme", 27));
+console.log(mostraCidade("Guilherme", "Belo Horizonte"));
+console.log(mostraHobby("Guilherme", "Jogar"));
