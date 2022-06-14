346e4ecc08fede9da890cc530f346aa0b41c3f42
GPMrks <guilhermepereiramarques@hotmail.com>
2022-06-14 19:21:03 -0300

    FEAT - Introdução a Javascript - Developed proposed challenges

--- Introducao-ao-Javascript/Javascript-em-uma-pagina-da-web/assets/js/scripts.js
+++ Introducao-ao-Javascript/Javascript-em-uma-pagina-da-web/assets/js/scripts.js
@@ -1,6 +1,12 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+const buttonAdd = document.getElementById("add");
+buttonAdd.addEventListener("click", increment);
+
+const buttonRemove = document.getElementById("remove");
+buttonRemove.addEventListener("click", decrement);
+
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
@@ -23,4 +29,14 @@ function checkValue(){
     } else {
         currentNumberWrapper.style.color = "black";
     }
+
+    if (currentNumber === 10) {
+        buttonAdd.disabled = true;
+    } else if (currentNumber === -10) {
+        buttonRemove.disabled = true;
+    } else {
+        buttonAdd.disabled = false;
+        buttonRemove.disabled = false;
+    }
+
 }
