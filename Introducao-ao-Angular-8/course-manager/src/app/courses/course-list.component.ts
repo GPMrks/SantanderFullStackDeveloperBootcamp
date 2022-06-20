ff291c5acbb726cbd38b81d2df303bf5c76fa687
GPMrks <guilhermepereiramarques@hotmail.com>
2022-06-20 16:54:20 -0300

    FEAT - Created a view for /Courses page | Created Edit page to edit courses informations

--- Introducao-ao-Angular-8/course-manager/src/app/courses/course-list.component.ts
+++ Introducao-ao-Angular-8/course-manager/src/app/courses/course-list.component.ts
@@ -1,67 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
 
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+
+    _filterBy!: string;
+
+    constructor(private courseService: CourseService) {
+
+    }
 
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'November, 2nd, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'December, 4th, 2019'
-            },
-            {
-                id: 3,
-                name: 'Angular: Router',
-                imageUrl: '/assets/images/router.png',
-                price: 75.99,
-                code: 'GTO-7412',
-                duration: 120,
-                rating: 5,
-                releaseDate: 'October, 1st, 2019'
-            },
-            {
-                id: 4,
-                name: 'Angular: CLI',
-                imageUrl: '/assets/images/cli.png',
-                price: 90.99,
-                code: 'CLI-1874',
-                duration: 60,
-                rating: 3,
-                releaseDate: 'June, 15th, 2019'
-            },
-            {
-                id: 5,
-                name: 'Angular: Animations',
-                imageUrl: '/assets/images/animations.png',
-                price: 80.99,
-                code: 'AMT-5214',
-                duration: 240,
-                rating: 5,
-                releaseDate: 'January, 24th, 2020'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
     }
 
 }
--- Introducao-ao-Angular-8/course-manager/src/app/courses/course.ts
+++ Introducao-ao-Angular-8/course-manager/src/app/courses/course.ts
@@ -1,6 +1,7 @@
 export class Course {
     id: number;
     name: string;
+    description: string;
     imageUrl: string;
     price: number;
     code: string;
@@ -8,9 +9,10 @@ export class Course {
     rating: number;
     releaseDate: string;
 
-    constructor(id: number, name: string, imageUrl: string, price: number, code: string, duration: number, rating: number, releaseDate: string){
+    constructor(id: number, name: string, description: string, imageUrl: string, price: number, code: string, duration: number, rating: number, releaseDate: string){
         this.id = id;
         this.name = name;
+        this.description = description;
         this.imageUrl = imageUrl;
         this.price = price;
         this.code = code;
