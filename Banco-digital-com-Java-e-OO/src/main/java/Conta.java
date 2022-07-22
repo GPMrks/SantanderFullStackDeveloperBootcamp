93ec6ffd5270feb30655ca24d38672e42e1d1abf
GPMrks <guilhermepereiramarques@hotmail.com>
2022-07-22 16:54:05 -0300

    ADD - Criando um Banco digital com Java e Programação Orientada a Objetos

---
+++ Banco-digital-com-Java-e-OO/src/main/java/Conta.java
@@ -0,0 +1,51 @@
+public abstract class Conta implements IConta{
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo = saldo - valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo = saldo + valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInfos() {
+        System.out.println("Cliente: " + this.cliente.getNome());
+        System.out.printf("Agência: %d", this.agencia);
+        System.out.printf("\nNúmero: %d", this.numero);
+        System.out.printf("\nSaldo: %.2f\n", this.saldo);
+    }
+
+}
---
+++ Banco-digital-com-Java-e-OO/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cliente guilherme = new Cliente();
+        guilherme.setNome("Guilherme Marques");
+
+        Conta cc = new ContaCorrente(guilherme);
+        Conta poupanca = new ContaPoupanca(guilherme);
+
+        cc.depositar(1000);
+        cc.transferir(300, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+
+    }
+}
